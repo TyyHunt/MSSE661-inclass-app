c09958449f7d107efe7c9914faada414fb620b18
TyyHunt <tyhunt04@gmail.com>
2020-11-15 18:50:26 -0700

    added about.html page

---
+++ week4/public/src/app.js
@@ -0,0 +1,27 @@
+var object = {
+ name: "Tyler",
+ age: 28
+}
+
+class Person {
+ name;
+ age;
+
+ constructor(name, age) {
+  this.name = name;
+  this.age = age;
+ }
+
+ getLegal() {
+  return this.age >= 21;
+ };
+
+ getName() {
+  return `Hello, my name is ${this.name}`;
+ }
+}
+
+let me = new Person('Tyler', 28);
+console.log(me.getName());
+
+
